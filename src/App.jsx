86a7c1e3c01bf94081014377ed3146dce47a7f07
Sferas379 <rthyssen73@gmail.com>
2022-03-02 23:22:47 +0100

    second one

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const url = "https://api.checkwx.com/station/KJFK/?x-api-key="
+  const key = "8cf71fb165574cd4a17423a33f"
+  
+  // console.log(url+key);
+  
+  
+  
+  
+  // const [infos, setInfos] = useState(false)
+  // const [arrayOfData, setArrayOfData] = useState([])
+
+  // useEffect(async()=>{
+  //   const data = await fetch("https://api.checkwx.com/station/KJFK/?x-api-key=8cf71fb165574cd4a17423a33f")
+  //   const result = await data.json()
+  //   setInfos(result)
+  //   setArrayOfData(true)
+  // }, [])
+
+  // if(infos){
+  //   console.log(arrayOfData);
+  // }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi there! </h1>
+        <ul>
+          {
+            this.state.
+          }
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
